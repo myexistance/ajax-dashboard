5668b4398687f098941c75e8f08f3afcc71d73e9
Hojoong Yoon <932400976@bun2-206-15.local>
2019-02-15 14:49:57 +1300

    External JSON example

---
+++ script2.js
@@ -0,0 +1,71 @@
+
+// Load the Visualization API and the controls package.
+google.charts.load('current', {'packages':['corechart', 'controls']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawDashboard);
+
+// Callback that creates and populates a data table,
+// instantiates a dashboard, a range slider and a pie chart,
+// passes in the data and draws it.
+function drawDashboard() {
+  $.ajax({
+    url: 'data.json',
+    dataType: 'json',
+    success: function(dataFromJSON){
+
+  console.log(dataFromJSON[0].name , dataFromJSON[0].Percentage) //arrayToDataTable example
+  // Create our data table.
+  // var data = google.visualization.arrayToDataTable([
+  //   ["Name", "Grade Percentage"],
+  //   [grades[0].name , grades[0].Percentage ],
+  //   [grades[1].name , grades[1].Percentage],
+  //   [grades[2].name , grades[2].Percentage],
+  //   [grades[3].name , grades[3].Percentage]
+  // ]);
+
+  var data = new google.visualization.DataTable(); // for loop example
+      data.addColumn('string','Name');
+      data.addColumn('number','Percentage');
+      for (var i=0; i<dataFromJSON.length; i++) {
+        data.addRow([dataFromJSON[i].name, dataFromJSON[i].Percentage]);
+      }
+
+
+
+  // Create a dashboard.
+  var dashboard = new google.visualization.Dashboard(
+      document.getElementById('dashboard_div'));
+
+  // Create a range slider, passing some options
+  var donutRangeSlider = new google.visualization.ControlWrapper({
+    'controlType': 'NumberRangeFilter',
+    'containerId': 'filter_div',
+    'options': {
+      'filterColumnLabel': 'Percentage'
+    }
+  });
+
+  // Create a pie chart, passing some options
+  var pieChart = new google.visualization.ChartWrapper({
+    'chartType': 'PieChart',
+    'containerId': 'chart_div',
+    'options': {
+      'width': 300,
+      'height': 300,
+      'pieSliceText': 'value',
+      'legend': 'right'
+    }
+  });
+
+  // Establish dependencies, declaring that 'filter' drives 'pieChart',
+  // so that the pie chart will only display entries that are let through
+  // given the chosen slider range.
+  dashboard.bind(donutRangeSlider, pieChart);
+
+  // Draw the dashboard.
+  dashboard.draw(data);
+
+}// function(dataFrom JSON)
+});//ajax
+}
